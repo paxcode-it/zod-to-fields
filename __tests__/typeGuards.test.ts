22e3095a81d3650acc4f009618a81c83ebc04e74
Wojciech Król <wojciechkrol97@gmail.com>
2023-10-04 13:01:34 +0200

    added more typeguards and tests

--- __tests__/typeGuards.test.ts
+++ __tests__/typeGuards.test.ts
@@ -6,12 +6,15 @@ import {
 import {
   isFormFieldsArray,
   isGenericSingleFieldOptions,
+  isInputBooleanFieldOptions,
+  isInputStringFieldOptions,
+  isObjectOfFormFieldsArrays,
 } from '@/utils/typeGuards'
 
 describe('isFormFieldsArray', () => {
   it('should return true for an empty array', () => {
     // Arrange
-    const arr: FormFieldsArray = []
+    const arr: FormFieldsArray = [{}, {}]
 
     // Act
     const result = isFormFieldsArray(arr)
@@ -22,11 +25,17 @@ describe('isFormFieldsArray', () => {
 
   it('should return true for a valid FormFieldsArray', () => {
     // Arrange
-    const arr: FormFieldsArray = [
-      {
-        /* some valid GenericFieldOptions */
-      },
-    ]
+    const arr: { [key: string]: FormFieldsArray } = {
+      someKy: [
+        {
+          name: 'someName',
+          label: 'someLabel',
+          id: 'someId',
+          tag: 'input',
+          type: 'text',
+        },
+      ],
+    }
 
     // Act
     const result = isFormFieldsArray(arr)
@@ -83,6 +92,114 @@ describe('isGenericSingleFieldOptions', () => {
     )
 
     // Assert
+    expect(result).toBe(false)
+  })
+})
+
+describe('isObjectOfFormFieldsArrays', () => {
+  it('should return true for an object containing FormFieldsArrays', () => {
+    const obj = {
+      key1: [{ tag: 'input', type: 'checkbox' }],
+      key2: [{ tag: 'input', type: 'text' }],
+    }
+
+    const result = isObjectOfFormFieldsArrays(obj)
+
+    expect(result).toBe(true)
+  })
+
+  it('should return false for non-object types', () => {
+    const nonObjectTypes: unknown[] = [null, undefined, 'string', 42]
+
+    nonObjectTypes.forEach(type => {
+      const result = isObjectOfFormFieldsArrays(type)
+
+      expect(result).toBe(false)
+    })
+  })
+})
+
+describe('isInputStringFieldOptions', () => {
+  it('should return true for valid string input types', () => {
+    const validTypes = [
+      'url',
+      'text',
+      'password',
+      'search',
+      'tel',
+      'email',
+    ] as const
+
+    validTypes.forEach(type => {
+      const field: GenericSingleFieldOptions = {
+        tag: 'input',
+        type,
+        name: 'someName',
+        id: 'someId',
+        label: 'someLabel',
+      }
+
+      const result = isInputStringFieldOptions(field)
+
+      expect(result).toBe(true)
+    })
+  })
+
+  it('should return false for invalid string input types', () => {
+    const field: GenericSingleFieldOptions = {
+      tag: 'input',
+      type: 'radio',
+      name: 'someName',
+      id: 'someId',
+      label: 'someLabel',
+    }
+
+    const result = isInputStringFieldOptions(field)
+
+    expect(result).toBe(false)
+  })
+})
+
+describe('isInputBooleanFieldOptions', () => {
+  it('should return true for checkbox input', () => {
+    const field: GenericSingleFieldOptions = {
+      tag: 'input',
+      type: 'checkbox',
+      name: 'someName',
+      id: 'someId',
+      label: 'someLabel',
+    }
+
+    const result = isInputBooleanFieldOptions(field)
+
+    expect(result).toBe(true)
+  })
+
+  it('should return true for radio input', () => {
+    const field: GenericSingleFieldOptions = {
+      tag: 'input',
+      type: 'radio',
+      name: 'someName',
+      id: 'someId',
+      label: 'someLabel',
+    }
+
+    const result = isInputBooleanFieldOptions(field)
+
+    expect(result).toBe(true)
+  })
+
+  it('should return false for non-boolean input types', () => {
+    const field: GenericSingleFieldOptions = {
+      tag: 'input',
+      type: 'text',
+      name: 'someName',
+      id: 'someId',
+      label: 'someLabel',
+    }
+
+    const result = isInputBooleanFieldOptions(field)
+
     expect(result).toBe(false)
   })
 })
--- src/utils/typeGuards.ts
+++ src/utils/typeGuards.ts
@@ -1,4 +1,8 @@
 import {
+  InputBooleanFieldOptions,
+  InputStringFieldOptions,
+} from '@/types/FieldOptions'
+import {
   FormFieldsArray,
   GenericFieldOptions,
   GenericSingleFieldOptions,
@@ -14,4 +18,32 @@ export function isGenericSingleFieldOptions(
   value: GenericFieldOptions
 ): value is GenericSingleFieldOptions {
   return !isFormFieldsArray(value)
+}
+
+export function isInputBooleanFieldOptions(
+  value: GenericSingleFieldOptions
+): value is InputBooleanFieldOptions {
+  return (
+    value.tag === 'input' &&
+    (value.type === 'checkbox' || value.type === 'radio')
+  )
+}
+
+export function isInputStringFieldOptions(
+  value: GenericSingleFieldOptions
+): value is InputStringFieldOptions {
+  return (
+    value.tag === 'input' &&
+    ['url', 'text', 'password', 'search', 'tel', 'email'].includes(value.type)
+  )
+}
+
+export function isObjectOfFormFieldsArrays(
+  value: unknown
+): value is { [key: string]: FormFieldsArray } {
+  if (typeof value !== 'object' || value === null) {
+    return false
+  }
+
+  return Object.values(value).every(isFormFieldsArray)
 }
